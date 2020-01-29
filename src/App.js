3538190f3ba066f74fa0647e38d253ca46481e8a
Telychko Vitalii <newme1989.5@gmail.com>
2020-01-29 22:50:48 +0200

    added view and business logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+      matrix: new Array(100).fill(null)
+  };
 
-export default App;
+  randomNumberInRange = (min, max) => {
+      const copy = this.state.matrix, newRandomNumber = Math.floor(Math.random() * (max - min) + min);
+      if(!this.state.matrix.includes(newRandomNumber)) {
+          copy[Math.floor(Math.random() * (max - min) + min)] = newRandomNumber;
+          this.setState({ matrix: copy });
+      } else {
+          return this.randomNumberInRange(1, 100);
+      }
+  };
+
+  start = () => {
+      this.interval = setInterval(() => {
+        return this.randomNumberInRange(1, 100);
+      }, 1000);
+  };
+
+  clear = () => {
+      clearInterval(this.interval);
+      this.setState({ matrix: this.state.matrix.map(value => {
+          value = '';
+          return value;
+      })
+      });
+  };
+
+  render() {
+      const size = 10;
+      return (
+          <>
+              <section id='buttons'>
+                  <button onClick={this.start}>Start</button>
+                  <button onClick={this.clear}>Clear</button>
+              </section>
+              <main id="matrix">
+                  {this.state.matrix.map((content, index) => {
+                      return <article className="cell" key={index}>{content}</article>;
+                  }).reduce((arr, element, index) => {
+                      index % size === 0 && arr.push([]);
+                      arr[arr.length - 1].push(element);
+                      return arr;
+                  }, []).map((row, index) => <section className="row" key={index}>{row}</section>)}
+              </main>
+          </>
+     )
+   }
+}
